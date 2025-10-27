e823c33ea9b7eaec3b06a7bceb98ea4c52c2ab61
afajohn <afacebu.john@gmail.com>
2025-10-27 17:11:46 +0800

    remove private files in the repository

--- supabase-storage-policy-simple.sql
+++
@@ -1,5 +0,0 @@
--- Ultra Simple Storage Policy - This should work without errors
--- Run this single statement in Supabase SQL Editor:
-
-CREATE POLICY "Allow profile photos access" ON storage.objects
-USING (bucket_id = 'profile-photos');
